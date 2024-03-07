72ab658e34fc240fa6069d428cc143455c724461
parisempress <parisempress@gmail.com>
2024-03-07 17:41:05 +0100

    Project 1

---
+++ WeSplitProject1/WeSplitProject1/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Rochelle Simone Lawrence on 06.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+
+    let tipPercentages = [10, 15, 20, 25, 0]
+
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = checkAmount/100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal/peopleCount
+        return amountPerPerson
+    }
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section("How much do you want to tip?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplitProject1/WeSplitProject1/WeSplitProject1App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitProject1App.swift
+//  WeSplitProject1
+//
+//  Created by Rochelle Simone Lawrence on 07.03.24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitProject1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
