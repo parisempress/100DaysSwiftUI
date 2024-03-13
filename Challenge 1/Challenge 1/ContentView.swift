4bfc70d63a7fa298194b675d17384aa46774f60f
parisempress <parisempress@gmail.com>
2024-03-13 18:22:17 +0100

    Reviewed Challenge

--- Challenge 1/Challenge 1/ContentView.swift
+++ Challenge 1/Challenge 1/ContentView.swift
@@ -8,51 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var inputValue: Double = 0.0
-    @State private var outputValue: Double = 0.0
-    @State private var unit: String = "meters"
+    @State private var input: Double = 100.0
+    @State private var inputUnit: String = "Meters"
+    @State private var outputUnit: String = "Kilometers"
 
-    let lengthOutputUnits = ["meters", "kilometers", "feet", "yard"]
-    let lengthInputUnits = ["meters", "kilometers", "feet", "yard"]
+    @FocusState private var inputIsFocused: Bool
 
-    var convertToMeters: Double {
-        return inputValue * 100
-    }
-    var convertToKilometer: Double {
-        return inputValue * 1000
-    }
-    var convertToFeet: Double {
-        return inputValue
-    }
-    var convertToYard: Double {
-        return inputValue
+
+    let units = ["Meters", "Kilometers", "Feet", "Yard", "Miles"]
+
+    var result: String {
+        let inputToMeterMultiplier: Double
+        let meterToOutputMultiplier: Double
+
+        switch inputUnit {
+        case "Feet":
+            inputToMeterMultiplier = 0.3048
+        case "Kilometers":
+            inputToMeterMultiplier = 1000
+        case "Miles":
+            inputToMeterMultiplier = 1609.34
+        case "Yards":
+            inputToMeterMultiplier = 0.9144
+        default:
+            inputToMeterMultiplier = 1.0
+        }
+
+        switch outputUnit {
+        case "Feet":
+            meterToOutputMultiplier = 3.28884
+        case "Kilometers":
+            meterToOutputMultiplier = 0.001
+        case "Miles":
+            meterToOutputMultiplier = 0.000621371
+        case "Yards":
+            meterToOutputMultiplier = 1.09361
+        default:
+            meterToOutputMultiplier = 1.0
+        }
+
+        let inputInMeters = input * inputToMeterMultiplier
+        let output = inputInMeters * meterToOutputMultiplier
+
+        let outputString = output.formatted()
+        return "\(outputString) \(outputUnit.lowercased())"
     }
 
     var body: some View {
         NavigationStack {
             Form {
-                TextField("Input", value: $inputValue, format: .number)
-                Section("Length Conversion") {
-                    Picker("Conversion", selection: $unit) {
-                        ForEach(lengthOutputUnits, id: \.self) { unit in
-                               Text(unit)
-                           }
-                    }
-                    .pickerStyle(.segmented)
+                Section("Amount to convert") {
+                    TextField("Input", value: $input, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($inputIsFocused)
                 }
 
-                Text("Output is \(convertToKilometer)")
-                Section("Length Conversion") {
-                    Picker("Conversion", selection: $unit) {
-                        ForEach(lengthInputUnits, id: \.self) { unit in
-                               Text(unit)
-                           }
+                Picker("Convert From", selection: $inputUnit) {
+                    ForEach(units, id: \.self) {
+                        Text($0)
                     }
-                    .pickerStyle(.segmented)
                 }
 
+                Picker("Convert To", selection: $outputUnit) {
+                    ForEach(units, id: \.self) {
+                        Text($0)
+                    }
+                }
+
+                Section("Result") {
+                    Text(result)
+                }
             }
             .navigationTitle("Unit Converter")
+            .toolbar {
+                if inputIsFocused {
+                    Button("Done") {
+                        inputIsFocused = false
+                    }
+                }
+            }
         }
 
     }
