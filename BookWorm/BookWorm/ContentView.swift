c2ec9dd12c20efc0c3b93a62fd9d54e3ac2fd4ce
parisempress <parisempress@gmail.com>
2024-08-02 15:25:37 +0200

    Add assets to Bookworm project

--- BookWorm/BookWorm/ContentView.swift
+++ BookWorm/BookWorm/ContentView.swift
@@ -26,6 +26,9 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Bookworm")
+            .navigationDestination(for: Book.self) { book in
+                DetailView(book: book)
+            }
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button("Add Book", systemImage: "plus") {
---
+++ BookWorm/BookWorm/DetailView.swift
@@ -0,0 +1,51 @@
+//
+//  DetailView.swift
+//  BookWorm
+//
+//  Created by Rochelle Simone Lawrence on 02.08.24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct DetailView: View {
+    let book: Book
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre)
+                    .resizable()
+                    .scaledToFit()
+                Text(book.genre.uppercased())
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundStyle(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                    .offset(x: -5, y: -5)
+            }
+            Text(book.author)
+                .font(.title)
+                .foregroundStyle(.secondary)
+            Text(book.review)
+                .padding()
+            RatingView(rating: .constant(book.rating))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title)
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+    }
+}
+
+#Preview {
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Book.self, configurations: config)
+        let example = Book(title: "Test Book", author: "Test Author", genre: "Fantasy", review: "This was a great book; I really enjoyed it", rating: 4)
+        return DetailView(book: example)
+            .modelContainer(container)
+    } catch {
+        return Text("Failed to create preview: \(error.localizedDescription)")
+    }
+}
